c9bad9440e49b4ac94e7acfa249a32791cee44d2
Andrey Kurosh <mail@spark.ms>
2017-01-08 19:25:49 +0300

    Whoops.

--- Corund/Behaviours/Movement/LineMovementBehaviour.cs
+++ Corund/Behaviours/Movement/LineMovementBehaviour.cs
@@ -22,14 +22,16 @@ namespace Corund.Behaviours.Movement
         /// <summary>
         /// Creates segments of the path.
         /// </summary>
-        protected override List<LineSegment> getSegments(IList<Vector2> points)
+        protected override List<LineSegment> GetSegments(IList<Vector2> points)
         {
             if(points.Count < 2)
                 throw new ArgumentException("Line must contain at least 2 points.");
 
             var result = new List<LineSegment>(points.Count - 1);
+
             for (var idx = 0; idx < points.Count - 1; idx++)
                 result.Add(new LineSegment(points[idx], points[idx + 1]));
+
             return result;
         }
     }
