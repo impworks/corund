1447e189452e1b62c1d6ff3cd69059af9ccc7671
unknown <Kurosh@c0770059.de-tech.ru>
2017-01-25 19:01:35 +0300

    ParticleGroup stub.

---
+++ Corund/Visuals/Particles/ParticleBase.cs
@@ -0,0 +1,64 @@
+﻿using Corund.Engine;
+using Corund.Visuals.Primitives;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Corund.Visuals.Particles
+{
+    /// <summary>
+    /// Base class for "thin" particles.
+    /// </summary>
+    public abstract class ParticleBase : DynamicObject
+    {
+        #region Constructor
+
+        protected ParticleBase(Texture2D texture, Vector2 hotSpot)
+        {
+            Texture = texture;
+            HotSpot = hotSpot;
+        }
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Current texture.
+        /// </summary>
+        public readonly Texture2D Texture;
+
+        /// <summary>
+        /// Offset from texture's top left coordinate that is the origin point for drawing.
+        /// </summary>
+        public readonly Vector2 HotSpot;
+
+        #endregion
+
+        #region Drawing
+
+        /// <summary>
+        /// Draws the particle to the screen.
+        /// </summary>
+        protected override void DrawInternal()
+        {
+            base.DrawInternal();
+
+            // sic! SpriteBatch.Begin is called once in ParticleGroup.DrawInternal
+
+            var transform = GetTransformInfo(true);
+            GameEngine.Render.SpriteBatch.Draw(
+                Texture,
+                transform.Position,
+                null,
+                Tint,
+                transform.Angle,
+                HotSpot,
+                transform.ScaleVector,
+                SpriteEffects.None,
+                GameEngine.Current.ZOrderFunction(this)
+            );
+        }
+
+        #endregion
+    }
+}
---
+++ Corund/Visuals/Particles/ParticleGroup.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using Corund.Engine;
+using Corund.Tools.Helpers;
+using Corund.Tools.Jitter;
+using Corund.Visuals.Primitives;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Corund.Visuals.Particles
+{
+    /// <summary>
+    /// A group of particle objects.
+    /// </summary>
+    public class ParticleGroup: ObjectGroup
+    {
+        #region Constructor
+
+        public ParticleGroup(int rate, Func<DynamicObject> factory)
+        {
+            GenerationRate = rate;
+            _particleFactory = factory;
+
+            BlendState = BlendState.AlphaBlend;
+        }
+
+        #endregion
+
+        #region Fields
+
+        /// <summary>
+        /// A function that creates particles.
+        /// </summary>
+        private readonly Func<DynamicObject> _particleFactory;
+
+        /// <summary>
+        /// Time elapsed since last particle has been created.
+        /// </summary>
+        private float _particleElapsedTime;
+
+        /// <summary>
+        /// Delay between two particles.
+        /// </summary>
+        private float _particleDelay;
+
+        /// <summary>
+        /// Number of particles per second to create.
+        /// </summary>
+        private int _generationRate;
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Flag indicating that the group actively generates new particles.
+        /// </summary>
+        public bool IsActive;
+
+        /// <summary>
+        /// Number of particles generated during the entire lifespan of the group.
+        /// </summary>
+        public int TotalParticleCount;
+
+        /// <summary>
+        /// Blend state for the particle system.
+        /// </summary>
+        public BlendState BlendState;
+
+        /// <summary>
+        /// Jittery location at which particles are created.
+        /// </summary>
+        public JitteryVector2 ParticleOrigin;
+
+        /// <summary>
+        /// Jittery angle of particle propulsion.
+        /// </summary>
+        public JitteryValue ParticleAngle;
+
+        /// <summary>
+        /// Jittery speed of particle propulsion.
+        /// </summary>
+        public JitteryValue ParticleSpeed;
+
+        /// <summary>
+        /// Jittery lifespan of each particle in seconds.
+        /// </summary>
+        public JitteryValue ParticleTimeToLive;
+
+        /// <summary>
+        /// Number of particles to create per second.
+        /// </summary>
+        public int GenerationRate
+        {
+            get { return _generationRate; }
+            set
+            {
+                _generationRate = value;
+                _particleDelay = 1.0f/value;
+            }
+        }
+
+        #endregion
+
+        #region Methods
+
+        public override void Update()
+        {
+            base.Update();
+
+            if (IsActive)
+            {
+                _particleElapsedTime += GameEngine.Delta;
+                while (_particleElapsedTime > _particleDelay)
+                {
+                    _particleElapsedTime -= _particleDelay;
+
+                    var newParticle = _particleFactory();
+                    ConfigureParticle(newParticle);
+                    Add(newParticle);
+
+                    TotalParticleCount++;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Draws the particle system.
+        /// </summary>
+        protected override void DrawInternal()
+        {
+            GameEngine.Render.TryBeginBatch(BlendState);
+
+            base.DrawInternal();
+        }
+
+        #endregion
+
+        #region Helpers
+
+        /// <summary>
+        /// Applies settings to a newly created particle.
+        /// </summary>
+        public virtual void ConfigureParticle(DynamicObject obj)
+        {
+            obj.Position = ParticleOrigin.GetValue();
+            obj.Momentum = VectorHelper.FromLength(ParticleSpeed.GetValue(), ParticleAngle.GetValue());
+
+            GameEngine.Current.Timeline.Add(ParticleTimeToLive.GetValue(), obj.FadeOut);
+        }
+
+        #endregion
+    }
+}
