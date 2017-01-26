b434269e6a711e1986a5f9701353bc84a2671638
Andrey Kurosh <mail@spark.ms>
2017-01-26 16:23:14 +0300

    TextureHelper.FillCircle

--- Corund/Tools/Helpers/TextureHelper.cs
+++ Corund/Tools/Helpers/TextureHelper.cs
@@ -13,12 +13,12 @@ namespace Corund.Tools.Helpers
         /// </summary>
         public static void FillRect(this Texture2D tex, Rectangle rect, Color color)
         {
-            var arr = new Color[rect.Width * rect.Height];
+            var pixels = new Color[rect.Width * rect.Height];
 
-            for (var idx = 0; idx < arr.Length; idx++)
-                arr[idx] = color;
+            for (var idx = 0; idx < pixels.Length; idx++)
+                pixels[idx] = color;
 
-            tex.SetData(0, rect, arr, 0, arr.Length);
+            tex.SetData(0, rect, pixels, 0, pixels.Length);
         }
 
         /// <summary>
@@ -28,6 +28,30 @@ namespace Corund.Tools.Helpers
         {
             var rect = new Rectangle(0, 0, tex.Width, tex.Height);
             tex.FillRect(rect, color);
+        }
+
+        /// <summary>
+        /// Fills a circle in the texture.
+        /// </summary>
+        public static void FillCircle(this Texture2D tex, float xCenter, float yCenter, float radius, Color color)
+        {
+            var rect = new Rectangle((int)(xCenter - radius), (int)(yCenter - radius), (int)(radius*2), (int)(radius*2));
+            var pixels = new Color[rect.Width * rect.Height];
+
+            tex.GetData(0, rect, pixels, 0, pixels.Length);
+
+            for (var x = 0; x < rect.Width; x++)
+            {
+                for (var y = 0; y < rect.Height; y++)
+                {
+                    var xRel = x + rect.Left - xCenter;
+                    var yRel = y + rect.Top - yCenter;
+                    if (xRel*xRel + yRel*yRel <= radius)
+                        pixels[y*rect.Width + x] = color;
+                }
+            }
+
+            tex.SetData(0, rect, pixels, 0, pixels.Length);
         }
     }
 }
