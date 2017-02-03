439283dc5aea2ed54b024d7830019f37741e2422
Andrey Kurosh <mail@spark.ms>
2017-02-03 18:59:32 +0300

    ParallaxBehaviour stub.

---
+++ Corund/Behaviours/Movement/ParallaxBehaviour.cs
@@ -0,0 +1,46 @@
+﻿using Corund.Visuals.Primitives;
+
+namespace Corund.Behaviours.Movement
+{
+    /// <summary>
+    /// Positions object relative to camera with a coefficient.
+    /// </summary>
+    public class ParallaxBehaviour: BehaviourBase
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Creates a new ParallaxBehaviour.
+        /// </summary>
+        /// <param name="coefficient">
+        /// Parallax coefficient.
+        /// 0 = object is static, never moves.
+        /// 1 = object moves as usual.
+        /// 2 = object moves twice as fast.
+        /// </param>
+        public ParallaxBehaviour(float coefficient = 1)
+        {
+            _coefficient = coefficient;
+        }
+
+        #endregion
+
+        #region Fields
+
+        /// <summary>
+        /// Parallax coefficient.
+        /// </summary>
+        private readonly float _coefficient;
+
+        #endregion
+
+        #region Methods
+
+        public override void UpdateObjectState(DynamicObject obj)
+        {
+            // todo
+        }
+
+        #endregion
+    }
+}
