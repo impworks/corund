470f9207ab2153b56a0f68b554f1c0629cf7d0a5
Andrei Kurosh <mail@spark.ms>
2017-09-24 11:07:35 +0300

    FrictionBehaviour.

---
+++ Corund/Behaviours/Movement/FrictionBehaviour.cs
@@ -0,0 +1,43 @@
+﻿using Corund.Tools.Helpers;
+using Corund.Visuals.Primitives;
+
+namespace Corund.Behaviours.Movement
+{
+    /// <summary>
+    /// Gradually decreases the object's speed.
+    /// </summary>
+    public class FrictionBehaviour: BehaviourBase
+    {
+        #region Constructor
+
+        public FrictionBehaviour(float friction)
+        {
+            Friction = friction;
+        }
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// The friction coefficient.
+        /// 0 = no friction.
+        /// 1 = complete stop.
+        /// </summary>
+        public float Friction;
+
+        #endregion
+
+        #region Methods
+
+        public override void UpdateObjectState(DynamicObject obj)
+        {
+            if (obj.Speed.IsAlmostNull())
+                return;
+
+            obj.Speed *= (1 - Friction);
+        }
+
+        #endregion
+    }
+}
